54ed17914bc9a31acf3f3be28c5556de101ce99a
ghost-kali <tamimofficial122005@gmail.com>
2023-11-18 20:04:43 +0530

    Video Player

--- VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.java
+++ VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView player = findViewById(R.id.Player);
+        player.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        MediaController mediaController = new MediaController(this);
+        player.setMediaController(mediaController);
+        mediaController.setAnchorView(player);
+        player.start();
     }
+
 }
